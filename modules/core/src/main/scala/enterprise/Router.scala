2134dbda47bd8e21f6265e502355da18516fb360
marcinzh <marcinzh@tutanota.com>
2025-09-08 16:13:44 +0200

    add Router.withPrefix

--- modules/core/src/main/scala/enterprise/Router.scala
+++ modules/core/src/main/scala/enterprise/Router.scala
@@ -10,6 +10,33 @@ object Router:
       case None => RouterEffect.empty
       case Some(x) => x
 
+
+  def apply[U <: RouterEffect & RequestEffect, V](pathPrefix: String)(f: PartialFunction[DSL, Response[V] !! U]): Response[V] !! U =
+    withPrefix(pathPrefix)(apply(f))
+
+
+  /** Alias for [[withPrefix]]. */
+  def apply[U <: RouterEffect & RequestEffect, V](pathPrefix: String)(body: Response[V] !! U): Response[V] !! U =
+    withPrefix(pathPrefix)(body)
+
+
+  /** Alias for [[withPrefixes]]. */
+  def apply[U <: RouterEffect & RequestEffect, V](pairs: (String, Response[V] !! U)*): Response[V] !! U =
+    withPrefixes(pairs*)
+
+
+  def withPrefix[U <: RouterEffect & RequestEffect, V](pathPrefix: String)(body: Response[V] !! U): Response[V] !! U =
+    withPrefixes(pathPrefix -> body)
+
+
+  def withPrefixes[U <: RouterEffect & RequestEffect, V](pairs: (String, Response[V] !! U)*): Response[V] !! U =
+    RequestEffect.asks(_.path).flatMap: path =>
+      val found = pairs.collectFirst { case (pathPrefix, body) if path.startsWith(pathPrefix) => (path.drop(pathPrefix.size), body)}
+      RouterEffect.fromOption(found)
+      .flatMap: (path2, body) =>
+        RequestEffect.localModify(_.copy(path = path2))(body)
+
+
   //@#@ experimental
   def lazily[U, V](f: PartialFunction[DSL, Response[V] !! U]): Option[Response[V] !! U] !! RequestEffect =
     Request.asks(r => f.lift(DSL(r)))
